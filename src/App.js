8c4dce88d913a28201576ac6665d81b9c8d495d8
Alisson <alisson.silveira@kbase.com.br>
2022-08-11 13:49:06 -0300

    Tabela finalizada

--- src/App.js
+++ src/App.js
@@ -23,7 +23,6 @@ const App = () => {
       setIsLoading(true)
       try {
         const response = await api.get('https://api.api-futebol.com.br/v1/campeonatos/10/tabela')
-        console.log(response.data)
         setTeams(response.data)
       } catch (error) {
         console.error(error)
@@ -40,39 +39,52 @@ const App = () => {
       {
         //HTML condicional, se está buscando da API retorna:
         isLoading ? (
-          <h1>DEMORADO DEMAIS SLK</h1>
+          <h1>Carregando...</h1>
         ) : (
           //se não, retorna o q já carregou
           <>
             <div className='header centralize'>
-              {/* gostei */}
               <h1>Brazukapp</h1>
             </div>
             <div className='content centralize'>
               <table border='3'>
                 <thead>
                   <tr>
-                    <td>Posição</td>
-                    <td>Nome</td>
-                    <td>Pontos</td>
+                    <td></td>
+                    <td>Time</td>
+                    <td>PT</td>
+                    <td>J</td>
+                    <td>V</td>
+                    <td>E</td>
+                    <td>D</td>
+                    <td>GP</td>
+                    <td>GC</td>
+                    <td>SG</td>
+                    <td>%</td>
+                    <td>Últimos Resultados</td>
                   </tr>
                 </thead>
                 <tbody>
                   {
-                    //aqui a mágica acontece, no meio do HTML podemos usar chaves pra inserir JS, exemplo
-                    console.log('créu kkkkk sehh loooko')
-                  }
-
-                  {
+                    //aqui a mágica acontece, no meio do HTML podemos usar chaves pra inserir JS, exemplo  
                     //ou da pra fazer uma coisa mais interessante, como fazer um loop por um array e retornar algo, inclusive HTML
                     //já q salvamos um array de dados no teams:
                     teams.map((team) => {
                       return (
                         //key é importante em um loop, mas a explicação fica pra próx
                         <tr key={team.time.time_id}>
-                          <td>{team.posicao}</td>
+                          <td className="rightAlign">{team.posicao}</td>
                           <td>{team.time.nome_popular}</td>
-                          <td>{team.pontos}</td>
+                          <td className="rightAlign">{team.pontos}</td>
+                          <td className="rightAlign">{team.jogos}</td>
+                          <td className="rightAlign">{team.vitorias}</td>
+                          <td className="rightAlign">{team.empates}</td>
+                          <td className="rightAlign">{team.derrotas}</td>
+                          <td className="rightAlign">{team.gols_pro}</td>
+                          <td className="rightAlign">{team.gols_contra}</td>
+                          <td className="rightAlign">{team.saldo_gols}</td>
+                          <td>{team.aproveitamento}</td>
+                          <td>{team.ultimos_jogos}</td>
                         </tr>
                       )
                     })
